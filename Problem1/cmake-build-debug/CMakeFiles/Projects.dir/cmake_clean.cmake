e3f1148663726bab794d5fbf46003bd27140bb87
Furkan Yaman <furkanyaman319@gmail.com>
2021-04-24 15:54:17 +0300

    fixed bugs

---
+++ Problem1/cmake-build-debug/CMakeFiles/Projects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projects.dir/main.c.obj"
+  "Projects.exe"
+  "Projects.exe.manifest"
+  "Projects.pdb"
+  "libProjects.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
